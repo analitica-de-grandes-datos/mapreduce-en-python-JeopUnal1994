7b2a1fa65db2a33f59524112653ed54d59b5d569
Jaime Esteban Ordoñez Perez <95106012+JeopUnal1994@users.noreply.github.com>
2022-06-28 12:24:47 -0500

    Add files via upload

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#! /usr/bin/ python3
+
+import sys
+
+def purpose_amount(x):
+    return x[2]
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(purpose_amount(result))
